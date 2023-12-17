0c9c5351a460dd10482d64dfb7018bc69d895b6a
marat2010 <marat.s.73@mail.ru>
2023-12-17 22:43:08 +0300

    Ok

--- Scripts/4_cert.sh
+++ Scripts/4_cert.sh
@@ -13,14 +13,14 @@ if [[ -z $PROJECT_NAME || -z $DOMAIN_NAME || -z $BOT_TOKEN || -z $EMAIL_SSL ]]; 
     exit
 fi
 
-#===================
+#======= Временно удаляем наши конфигурации Nginx ===========
 rm -f /etc/nginx/sites-enabled/nginx_bot
 rm -f /etc/nginx/sites-enabled/$DOMAIN_NAME.conf
 echo
 echo "=== Перезапуск Nginx ==="
 systemctl daemon-reload
 systemctl restart nginx.service
-#===================
+#=============================================================
 
 echo
 echo "=== Установка пакета acme-nginx ===" 
@@ -38,17 +38,18 @@ acme-nginx -d $DOMAIN_NAME --debug
 wait
 #sleep 2
 
-#===================
+#======= Восстановим наши конфигурации Nginx ===============
 ln -s /etc/nginx/sites-available/nginx_bot.conf /etc/nginx/sites-enabled/nginx_bot
 ln -s /etc/nginx/sites-available/$DOMAIN_NAME.conf /etc/nginx/sites-enabled/$DOMAIN_NAME.conf
-#===================
+#===========================================================
 
-#rm -f /etc/ssl/nginx/$DOMAIN_NAME.crt
-#rm -f /etc/ssl/nginx/$DOMAIN_NAME.key
-#ln -s /etc/ssl/private/letsencrypt-domain.pem /etc/ssl/nginx/$DOMAIN_NAME.crt
-#ln -s /etc/ssl/private/letsencrypt-domain.key /etc/ssl/nginx/$DOMAIN_NAME.key
+#======= Делаем ссылки на новые сертификаты для Nginx ======
+rm -f /etc/ssl/nginx/$DOMAIN_NAME.crt
+rm -f /etc/ssl/nginx/$DOMAIN_NAME.key
+ln -s /etc/ssl/private/letsencrypt-domain.pem /etc/ssl/nginx/$DOMAIN_NAME.crt
+ln -s /etc/ssl/private/letsencrypt-domain.key /etc/ssl/nginx/$DOMAIN_NAME.key
+#==========================================================
 
-#==================================
 echo
 echo "=== Установка диспетчера задач для автоматического обноления SSL сертификата для Домена ===" 
 
@@ -68,10 +69,7 @@ echo "=== Адрес WEBHOOK_URL: https://$DOMAIN_NAME:8443/$PROJECT_NAME ==="
 echo "=== Проверка состояния ТГ вебхука: https://api.telegram.org/bot$BOT_TOKEN/getWebhookInfo ==="
 
 
-#====================================
-#mkdir -p /var/www/html/.well-known/pki-validation
-
-#======================================
+#==============================================================================
 #cat certificate.crt ca_bundle.crt >> $DOMAIN_NAME.crt
 #wget -O -  https://get.acme.sh | sh -s email=$EMAIL_ZEROSSL
 #~/.acme.sh/acme.sh  --issue  -d $DOMAIN_NAME  --nginx /etc/nginx/sites-available/nginx_bot.conf -m $EMAIL_ZEROSSL --server zerossl
